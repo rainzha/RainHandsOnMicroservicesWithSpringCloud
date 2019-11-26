64f1cda308cea1f30ebfc133c4981d9ab53b0b30
rainzha <rainzha@163.com>
2019-11-26 09:48:43 +0800

    实现ProductService微服务API

--- microservices/product-service/src/main/java/org/rainzha/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/org/rainzha/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package org.rainzha.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.rainzha")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
---
+++ microservices/product-service/src/main/java/org/rainzha/microservices/core/product/services/ProductServiceImpl.java
@@ -0,0 +1,35 @@
+package org.rainzha.microservices.core.product.services;
+
+import org.rainzha.api.core.product.Product;
+import org.rainzha.api.core.product.ProductService;
+import org.rainzha.util.exceptions.InvalidInputException;
+import org.rainzha.util.exceptions.NotFoundException;
+import org.rainzha.util.http.ServiceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
+
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    }
+}
