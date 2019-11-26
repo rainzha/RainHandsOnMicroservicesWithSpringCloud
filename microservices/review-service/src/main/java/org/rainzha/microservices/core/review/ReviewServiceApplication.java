a5b079ad6da7994c0cb4c9698128f1b9f73f0a97
rainzha <rainzha@163.com>
2019-11-26 10:09:48 +0800

    实现ReviewService微服务API

--- microservices/review-service/src/main/java/org/rainzha/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/org/rainzha/microservices/core/review/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package org.rainzha.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.rainzha")
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
