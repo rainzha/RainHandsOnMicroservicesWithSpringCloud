02c1500509d32089cbcf0e7378994bd3c91eb3f5
rainzha <rainzha@163.com>
2019-11-26 20:35:15 +0800

    实现ReviewService微服务API的Http测试

--- microservices/review-service/src/test/java/org/rainzha/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/org/rainzha/microservices/core/review/ReviewServiceApplicationTests.java
@@ -1,13 +1,75 @@
 package org.rainzha.microservices.core.review;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWebTestClient
 class ReviewServiceApplicationTests {
+    @Test
+    void getReviewsByProductId(@Autowired WebTestClient webClient) {
+        int productId = 1;
 
-	@Test
-	void contextLoads() {
-	}
+        webClient.get()
+                .uri("/review?productId=" + productId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(3)
+                .jsonPath("$[0].productId").isEqualTo(productId);
+    }
 
+    @Test
+    void getReviewsMissingParameter(@Autowired WebTestClient webClient) {
+        webClient.get()
+                .uri("/review")
+                .exchange()
+                .expectStatus().isEqualTo(BAD_REQUEST)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/review")
+                .jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present");
+    }
+
+    @Test
+    void getReviewsInvalidParameter(@Autowired WebTestClient webClient) {
+        webClient.get()
+                .uri("/review?productId=no-integer")
+                .exchange()
+                .expectStatus().isEqualTo(BAD_REQUEST)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/review")
+                .jsonPath("$.message").isEqualTo("Type mismatch.");
+    }
+
+    @Test
+    void getReviewsNotFound(@Autowired WebTestClient webClient) {
+        int productIdNotFound = 213;
+
+        webClient.get()
+                .uri("/review?productId=" + productIdNotFound)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(0);
+    }
+
+    @Test
+    void getReviewsInvalidParameterNegativeValue(@Autowired WebTestClient webClient) {
+        int productIdInvalid = -1;
+
+        webClient.get()
+                .uri("/review?productId=" + productIdInvalid)
+                .exchange()
+                .expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/review")
+                .jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
+    }
 }
