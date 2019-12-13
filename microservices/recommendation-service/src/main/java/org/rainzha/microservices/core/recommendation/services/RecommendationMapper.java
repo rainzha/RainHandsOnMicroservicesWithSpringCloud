9176968f7d3895f489c0836292e1024c0435001c
rainzha <rainzha@163.com>
2019-12-13 19:12:02 +0800

    使用持久层

---
+++ microservices/recommendation-service/src/main/java/org/rainzha/microservices/core/recommendation/services/RecommendationMapper.java
@@ -0,0 +1,29 @@
+package org.rainzha.microservices.core.recommendation.services;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.rainzha.api.core.recommendation.Recommendation;
+import org.rainzha.microservices.core.recommendation.persistence.RecommendationEntity;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RecommendationMapper {
+
+    @Mappings({
+            @Mapping(target = "rate", source = "entity.rating"),
+            @Mapping(target = "serviceAddress", ignore = true)
+    })
+    Recommendation entityToApi(RecommendationEntity entity);
+
+    @Mappings({
+            @Mapping(target = "rating", source = "api.rate"),
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "version", ignore = true)
+    })
+    RecommendationEntity apiToEntity(Recommendation api);
+
+    List<Recommendation> entityListToApiList(List<RecommendationEntity> entity);
+    List<RecommendationEntity> apiListToEntityList(List<Recommendation> api);
+}
